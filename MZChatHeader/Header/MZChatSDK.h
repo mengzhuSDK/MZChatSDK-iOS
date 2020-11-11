afc56186d33fa178c2915fb5c617ecbb99e706e8
libaoxi <libaoxi@yuelvhui.com>
2020-11-11 11:27:24 +0800

    version_2.6.6

---
+++ MZChatHeader/Header/MZChatSDK.h
@@ -0,0 +1,50 @@
+//
+//  MZChatSDK.h
+//  MZChatSDK
+//
+//  Created by LiWei on 2020/8/4.
+//  Copyright © 2020 mengzhu.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - SDK
+#import "MZChatKitManager.h"
+#import "MZChatApiManager.h"
+
+#pragma mark - APP
+#import "MZAppChatApiManager.h"
+#import "MZChatNetOperate.h"
+#import "MZChatThemeManager.h"
+#import "MZChatTool.h"
+#import "MZLongPolling.h"
+#import "MZChatPresenter.h"
+
+
+#import "DXFaceView.h"
+#import "FacialView.h"
+#import "MZAppMessageToolView.h"
+#import "MZBarrageSwitch.h"
+#import "MZLongPollDataModel.h"
+#import "MZRightModel.h"
+#import "MZGoodsListOuterModel.h"
+#import "MZGoodsListModel.h"
+#import "MZSingleContentModel.h"
+#import "MZGlobalContentModel.h"
+
+#import "HDAutoADModel.h"
+#import "HDCustomTextADModel.h"
+#import "MZRightContenModel.h"
+#import "MZRightModel.h"
+#import "MZActMsg.h"
+
+#import "MZChatCommen.h"
+
+//! Project version number for MZChatSDK.
+FOUNDATION_EXPORT double MZChatSDKVersionNumber;
+
+//! Project version string for MZChatSDK.
+FOUNDATION_EXPORT const unsigned char MZChatSDKVersionString[];
+
+
+
--- MZChatSDK.podspec
+++ MZChatSDK.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   s.authors      = "Mengzhu"
   s.name         = "MZChatSDK"
-  s.version      = "2.6.5"
+  s.version      = "2.6.6"
   s.summary      = "MZChatSDK MZ iOS SDK"
   s.description  = "Provides the user to push the stream and the playback function"
   s.homepage     = "http://www.zmengzhu.com"
@@ -15,4 +15,6 @@ Pod::Spec.new do |s|
   s.dependency 'MZCoreSDKLibrary','>= 2.6.0'
   s.dependency 'MZSocketIO'
   s.dependency 'MZEmojiLabel','~> 1.1.4'
+  s.source_files = 'MZChatHeader/Header/*'
+  s.public_header_files = 'MZChatHeader/Header/*.h'
   end
